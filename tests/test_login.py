d1d54fc6e021c438817a047f566c5c03cde93b47
Admin <yurasurak@gmail.com>
2024-05-23 09:27:09 +0300

    add commit

--- tests/test_login.py
+++ tests/test_login.py
@@ -5,13 +5,10 @@ from pages.login_page import LoginPage
 
 class TestLogin(unittest.TestCase):
     def test_blog_page_title(self):
-        # Your setup code (opening the page, filling in credentials) might go here
 
-        # Step 4: Click on "BLOG" tab
         login_page = LoginPage()
         blog_page = login_page.click_on_blog_tab()
 
-        # Verification
         expected_title = "Headless Browser Testing with Selenium: Elevate Your Expertise"
         actual_title = blog_page.get_title()
         self.assertEqual(actual_title, expected_title, "Title doesn't match")
